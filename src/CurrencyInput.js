ed996201b4cefa9de130ba61ffbe0a9287965f7c
Gintautas <gintautas.kondrasevicius@gmail.com>
2022-03-01 13:13:33 +0200

    Currencies added

---
+++ src/CurrencyInput.js
@@ -0,0 +1,15 @@
+import './currencyInput.css';
+
+function CurrencyInput(props) {
+  return (
+    <div className="group">
+      <input type="text" value={props.amount} onChange={ev => props.onAmountChange(ev.target.value)} />
+      <select value={props.currency} onChange={ev => props.onCurrencyChange(ev.target.value)}>
+        {props.currencies.map((currency => (
+          <option value={currency}>{currency}</option>
+        )))}
+      </select>
+    </div>
+  );
+}
+export default CurrencyInput;
